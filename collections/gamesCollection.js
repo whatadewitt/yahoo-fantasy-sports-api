f1c4cfba82cec3c07612d82f214ece1ce29df967
Luke DeWitt <luke.dewitt@redspace.com>
2017-11-13 21:28:24 -0400

    game resource

--- collections/gamesCollection.js
+++ collections/gamesCollection.js
@@ -1,7 +1,7 @@
-var _ = require('lodash');
-var gameHelper = require('../helpers/gameHelper.js');
+// var _ = require("lodash");
+// var gameHelper = require("../helpers/gameHelper.js");
 
-module.exports = GamesCollection;
+export default GamesCollection;
 
 function GamesCollection(yf) {
   this.yf = yf;
@@ -9,13 +9,13 @@ function GamesCollection(yf) {
 
 // params: game keys or filters, subresources (optional), callback
 GamesCollection.prototype.fetch = function() {
-  var gameKeys = '',
+  var gameKeys = "",
     subresources = [],
     filters = {},
     cb = arguments[arguments.length - 1];
 
   // there should be a better way...
-  if ( _.isObject(arguments[0]) ) {
+  if (_.isObject(arguments[0])) {
     // filters
     filters = arguments[0];
   } else {
@@ -25,47 +25,49 @@ GamesCollection.prototype.fetch = function() {
 
   subresources = arguments[1];
 
-  var url = 'https://fantasysports.yahooapis.com/fantasy/v2/games';
+  var url = "https://fantasysports.yahooapis.com/fantasy/v2/games";
 
-  if ( _.isString(gameKeys)  && !_.isEmpty(gameKeys) ) {
+  if (_.isString(gameKeys) && !_.isEmpty(gameKeys)) {
     gameKeys = [gameKeys];
   }
 
-  if ( !(_.isEmpty(gameKeys)) ) {
-    url += ';game_keys=' + gameKeys.join(',');
+  if (!_.isEmpty(gameKeys)) {
+    url += ";game_keys=" + gameKeys.join(",");
   }
 
-  if ( !( _.isEmpty(filters) )  ) {
+  if (!_.isEmpty(filters)) {
     _.each(Object.keys(filters), function(key) {
-      url += ';' + key + '=' + filters[key];
+      url += ";" + key + "=" + filters[key];
     });
   }
 
-  if ( _.isString(subresources) && !_.isEmpty(subresources) ) {
+  if (_.isString(subresources) && !_.isEmpty(subresources)) {
     subresources = [subresources];
   }
 
-  if ( !(_.isEmpty(subresources)) ) {
-    url += ';out=' + subresources.join(',');
+  if (!_.isEmpty(subresources)) {
+    url += ";out=" + subresources.join(",");
   }
 
-  url += '?format=json';
+  url += "?format=json";
 
   var apiCallback = this._fetch_callback.bind(this, cb, subresources);
 
-  this
-    .yf
-    .api(
-      this.yf.GET,
-      url,
-      apiCallback
-    );
+  this.yf.api(this.yf.GET, url, apiCallback);
 };
 
-GamesCollection.prototype._fetch_callback = function(cb, subresources, e, data) {
-  if ( e ) return cb(e);
-  
-  var games = gameHelper.parseCollection(data.fantasy_content.games, subresources);
+GamesCollection.prototype._fetch_callback = function(
+  cb,
+  subresources,
+  e,
+  data
+) {
+  if (e) return cb(e);
+
+  var games = gameHelper.parseCollection(
+    data.fantasy_content.games,
+    subresources
+  );
   return cb(null, games);
 };
 
@@ -77,7 +79,7 @@ GamesCollection.prototype.user = function() {
 
   switch (arguments.length) {
     case 2:
-      if ( _.isObject(arguments[0]) ) {
+      if (_.isObject(arguments[0])) {
         filters = arguments[0];
       } else {
         subresources = arguments[0];
@@ -94,78 +96,79 @@ GamesCollection.prototype.user = function() {
       break;
   }
 
-  var url = 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games';
+  var url =
+    "https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games";
 
-  if ( !( _.isEmpty(filters) )  ) {
+  if (!_.isEmpty(filters)) {
     _.each(Object.keys(filters), function(key) {
-      url += ';' + key + '=' + filters[key];
+      url += ";" + key + "=" + filters[key];
     });
   }
 
-  if ( _.isString(subresources) && !_.isEmpty(subresources) ) {
+  if (_.isString(subresources) && !_.isEmpty(subresources)) {
     subresources = [subresources];
   }
 
-  if ( !(_.isEmpty(subresources)) ) {
-    url += ';out=' + subresources.join(',');
+  if (!_.isEmpty(subresources)) {
+    url += ";out=" + subresources.join(",");
   }
 
-  url += '?format=json';
+  url += "?format=json";
 
   var apiCallback = this._user_callback.bind(this, cb, subresources);
 
-  this
-    .yf
-    .api(
-      this.yf.GET,
-      url,
-      apiCallback
-    );
+  this.yf.api(this.yf.GET, url, apiCallback);
 };
 
 GamesCollection.prototype._user_callback = function(cb, subresources, e, data) {
-  if ( e ) return cb(e);
-  
-  var games = gameHelper.parseCollection(data.fantasy_content.users[0].user[1].games, subresources);
+  if (e) return cb(e);
+
+  var games = gameHelper.parseCollection(
+    data.fantasy_content.users[0].user[1].games,
+    subresources
+  );
   return cb(null, games);
 };
 
 GamesCollection.prototype.userFetch = function() {
   // no filters...
   var gameKeys = arguments[0],
-    subresources = ( arguments.length > 2 ) ? arguments[1] : [],
+    subresources = arguments.length > 2 ? arguments[1] : [],
     cb = arguments[arguments.length - 1];
 
-  var url = 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=';
+  var url =
+    "https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=";
 
-  if ( _.isString(gameKeys) ) {
+  if (_.isString(gameKeys)) {
     gameKeys = [gameKeys];
   }
 
-  url += gameKeys.join(',');
+  url += gameKeys.join(",");
 
-  if ( !(_.isEmpty(subresources)) ) {
-    url += ';out=' + subresources.join(',');
+  if (!_.isEmpty(subresources)) {
+    url += ";out=" + subresources.join(",");
   }
 
-  url += '?format=json';
+  url += "?format=json";
 
   var apiCallback = this._userFetch_callback.bind(this, cb, subresources);
 
-  this
-    .yf
-    .api(
-      this.yf.GET,
-      url,
-      apiCallback
-    );
+  this.yf.api(this.yf.GET, url, apiCallback);
 };
 
-GamesCollection.prototype._userFetch_callback = function(cb, subresources, e, data) {
-  if ( e ) return cb(e);
-  
+GamesCollection.prototype._userFetch_callback = function(
+  cb,
+  subresources,
+  e,
+  data
+) {
+  if (e) return cb(e);
+
   var user = data.fantasy_content.users[0].user[0];
-  var games = gameHelper.parseCollection(data.fantasy_content.users[0].user[1].games, subresources);
+  var games = gameHelper.parseCollection(
+    data.fantasy_content.users[0].user[1].games,
+    subresources
+  );
   user.games = games;
 
   return cb(null, user);
