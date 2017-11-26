8c2fd5cca9b11239990c72edbe4338ac8a905e8a
Luke DeWitt <luke.dewitt@redspace.com>
2017-11-25 21:52:44 -0400

    league and transaction resources

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 /* global module, require */
 require = require("@std/esm")(module, true);
 module.exports = require("./YahooFantasy.mjs").default;
+
+// TODO: league settings sample data
+// TODO: transactions sample data
