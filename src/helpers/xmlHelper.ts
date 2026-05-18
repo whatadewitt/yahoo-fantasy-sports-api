44b371b592844450d334db98bd77da58899c9d84
Luke DeWitt <luke.dewitt!@redspace.com>
2026-05-18 08:52:00 -0300

    feat: add buildRosterPayload XML builder

--- src/helpers/xmlHelper.ts
+++ src/helpers/xmlHelper.ts
@@ -6,3 +6,34 @@ export function escapeXml(value: string | number): string {
     .replace(/"/g, "&quot;")
     .replace(/'/g, "&apos;");
 }
+
+const DECL = "<?xml version='1.0'?>";
+
+export type RosterCoverage = { week: number } | { date: string };
+export interface RosterSlot {
+  player_key: string;
+  position: string;
+}
+
+export function buildRosterPayload(
+  coverage: RosterCoverage,
+  players: RosterSlot[]
+): string {
+  const cov =
+    "week" in coverage
+      ? `<coverage_type>week</coverage_type><week>${escapeXml(
+          coverage.week
+        )}</week>`
+      : `<coverage_type>date</coverage_type><date>${escapeXml(
+          coverage.date
+        )}</date>`;
+  const playerEls = players
+    .map(
+      (p) =>
+        `<player><player_key>${escapeXml(
+          p.player_key
+        )}</player_key><position>${escapeXml(p.position)}</position></player>`
+    )
+    .join("");
+  return `${DECL}<fantasy_content><roster>${cov}<players>${playerEls}</players></roster></fantasy_content>`;
+}
--- tests/xmlHelper.spec.js
+++ tests/xmlHelper.spec.js
@@ -1,4 +1,4 @@
-const { escapeXml } = require("../dist/cjs/helpers/xmlHelper");
+const { escapeXml, buildRosterPayload } = require("../dist/cjs/helpers/xmlHelper");
 
 describe("xmlHelper.escapeXml", function () {
   it("escapes XML metacharacters", function () {
@@ -9,5 +9,33 @@ describe("xmlHelper.escapeXml", function () {
 
   it("stringifies non-strings", function () {
     expect(escapeXml(10)).toBe("10");
+  });
+});
+
+describe("xmlHelper.buildRosterPayload", function () {
+  it("builds a week-coverage roster body", function () {
+    const xml = buildRosterPayload({ week: 10 }, [
+      { player_key: "nfl.p.1", position: "WR" },
+      { player_key: "nfl.p.2", position: "BN" },
+    ]);
+    expect(xml).toBe(
+      "<?xml version='1.0'?><fantasy_content><roster>" +
+        "<coverage_type>week</coverage_type><week>10</week><players>" +
+        "<player><player_key>nfl.p.1</player_key><position>WR</position></player>" +
+        "<player><player_key>nfl.p.2</player_key><position>BN</position></player>" +
+        "</players></roster></fantasy_content>"
+    );
+  });
+
+  it("builds a date-coverage roster body", function () {
+    const xml = buildRosterPayload({ date: "2026-05-17" }, [
+      { player_key: "nba.p.9", position: "PG" },
+    ]);
+    expect(xml).toBe(
+      "<?xml version='1.0'?><fantasy_content><roster>" +
+        "<coverage_type>date</coverage_type><date>2026-05-17</date><players>" +
+        "<player><player_key>nba.p.9</player_key><position>PG</position></player>" +
+        "</players></roster></fantasy_content>"
+    );
   });
 });
