b465c9b8e5279c119ef913aad630c8aa4effcdba
Luke DeWitt <luke.dewitt!@redspace.com>
2026-05-18 13:32:11 -0300

    feat: add add/drop/waiver XML builders

--- src/helpers/xmlHelper.ts
+++ src/helpers/xmlHelper.ts
@@ -37,3 +37,82 @@ export function buildRosterPayload(
     .join("");
   return `${DECL}<fantasy_content><roster>${cov}<players>${playerEls}</players></roster></fantasy_content>`;
 }
+
+function addPlayerEl(playerKey: string, teamKey: string): string {
+  return `<player><player_key>${escapeXml(
+    playerKey
+  )}</player_key><transaction_data><type>add</type><destination_team_key>${escapeXml(
+    teamKey
+  )}</destination_team_key></transaction_data></player>`;
+}
+
+function dropPlayerEl(playerKey: string, teamKey: string): string {
+  return `<player><player_key>${escapeXml(
+    playerKey
+  )}</player_key><transaction_data><type>drop</type><source_team_key>${escapeXml(
+    teamKey
+  )}</source_team_key></transaction_data></player>`;
+}
+
+export function buildAddPayload(
+  addPlayerKey: string,
+  teamKey: string
+): string {
+  return `${DECL}<fantasy_content><transaction><type>add</type>${addPlayerEl(
+    addPlayerKey,
+    teamKey
+  )}</transaction></fantasy_content>`;
+}
+
+export function buildDropPayload(
+  dropPlayerKey: string,
+  teamKey: string
+): string {
+  return `${DECL}<fantasy_content><transaction><type>drop</type>${dropPlayerEl(
+    dropPlayerKey,
+    teamKey
+  )}</transaction></fantasy_content>`;
+}
+
+export function buildAddDropPayload(
+  addPlayerKey: string,
+  dropPlayerKey: string,
+  teamKey: string
+): string {
+  return `${DECL}<fantasy_content><transaction><type>add/drop</type><players>${addPlayerEl(
+    addPlayerKey,
+    teamKey
+  )}${dropPlayerEl(
+    dropPlayerKey,
+    teamKey
+  )}</players></transaction></fantasy_content>`;
+}
+
+export interface WaiverOptions {
+  faab_bid?: number;
+  drop_player_key?: string;
+}
+
+export function buildWaiverPayload(
+  addPlayerKey: string,
+  teamKey: string,
+  opts: WaiverOptions = {}
+): string {
+  const faab =
+    opts.faab_bid !== undefined
+      ? `<faab_bid>${escapeXml(opts.faab_bid)}</faab_bid>`
+      : "";
+  if (opts.drop_player_key) {
+    return `${DECL}<fantasy_content><transaction><type>add/drop</type>${faab}<players>${addPlayerEl(
+      addPlayerKey,
+      teamKey
+    )}${dropPlayerEl(
+      opts.drop_player_key,
+      teamKey
+    )}</players></transaction></fantasy_content>`;
+  }
+  return `${DECL}<fantasy_content><transaction><type>add</type>${faab}${addPlayerEl(
+    addPlayerKey,
+    teamKey
+  )}</transaction></fantasy_content>`;
+}
--- tests/xmlHelper.spec.js
+++ tests/xmlHelper.spec.js
@@ -1,4 +1,11 @@
-const { escapeXml, buildRosterPayload } = require("../dist/cjs/helpers/xmlHelper");
+const {
+  escapeXml,
+  buildRosterPayload,
+  buildAddPayload,
+  buildDropPayload,
+  buildAddDropPayload,
+  buildWaiverPayload,
+} = require("../dist/cjs/helpers/xmlHelper");
 
 describe("xmlHelper.escapeXml", function () {
   it("escapes XML metacharacters", function () {
@@ -36,6 +43,71 @@ describe("xmlHelper.buildRosterPayload", function () {
         "<coverage_type>date</coverage_type><date>2026-05-17</date><players>" +
         "<player><player_key>nba.p.9</player_key><position>PG</position></player>" +
         "</players></roster></fantasy_content>"
+    );
+  });
+});
+
+describe("xmlHelper transaction create builders", function () {
+  it("buildAddPayload", function () {
+    expect(buildAddPayload("nfl.p.1", "nfl.l.1.t.1")).toBe(
+      "<?xml version='1.0'?><fantasy_content><transaction>" +
+        "<type>add</type><player><player_key>nfl.p.1</player_key>" +
+        "<transaction_data><type>add</type>" +
+        "<destination_team_key>nfl.l.1.t.1</destination_team_key>" +
+        "</transaction_data></player></transaction></fantasy_content>"
+    );
+  });
+
+  it("buildDropPayload", function () {
+    expect(buildDropPayload("nfl.p.2", "nfl.l.1.t.1")).toBe(
+      "<?xml version='1.0'?><fantasy_content><transaction>" +
+        "<type>drop</type><player><player_key>nfl.p.2</player_key>" +
+        "<transaction_data><type>drop</type>" +
+        "<source_team_key>nfl.l.1.t.1</source_team_key>" +
+        "</transaction_data></player></transaction></fantasy_content>"
+    );
+  });
+
+  it("buildAddDropPayload", function () {
+    expect(buildAddDropPayload("nfl.p.1", "nfl.p.2", "nfl.l.1.t.1")).toBe(
+      "<?xml version='1.0'?><fantasy_content><transaction>" +
+        "<type>add/drop</type><players>" +
+        "<player><player_key>nfl.p.1</player_key><transaction_data>" +
+        "<type>add</type><destination_team_key>nfl.l.1.t.1</destination_team_key>" +
+        "</transaction_data></player>" +
+        "<player><player_key>nfl.p.2</player_key><transaction_data>" +
+        "<type>drop</type><source_team_key>nfl.l.1.t.1</source_team_key>" +
+        "</transaction_data></player>" +
+        "</players></transaction></fantasy_content>"
+    );
+  });
+
+  it("buildWaiverPayload with faab and no drop", function () {
+    expect(buildWaiverPayload("nfl.p.1", "nfl.l.1.t.1", { faab_bid: 17 })).toBe(
+      "<?xml version='1.0'?><fantasy_content><transaction>" +
+        "<type>add</type><faab_bid>17</faab_bid>" +
+        "<player><player_key>nfl.p.1</player_key><transaction_data>" +
+        "<type>add</type><destination_team_key>nfl.l.1.t.1</destination_team_key>" +
+        "</transaction_data></player></transaction></fantasy_content>"
+    );
+  });
+
+  it("buildWaiverPayload with faab and drop", function () {
+    expect(
+      buildWaiverPayload("nfl.p.1", "nfl.l.1.t.1", {
+        faab_bid: 5,
+        drop_player_key: "nfl.p.2",
+      })
+    ).toBe(
+      "<?xml version='1.0'?><fantasy_content><transaction>" +
+        "<type>add/drop</type><faab_bid>5</faab_bid><players>" +
+        "<player><player_key>nfl.p.1</player_key><transaction_data>" +
+        "<type>add</type><destination_team_key>nfl.l.1.t.1</destination_team_key>" +
+        "</transaction_data></player>" +
+        "<player><player_key>nfl.p.2</player_key><transaction_data>" +
+        "<type>drop</type><source_team_key>nfl.l.1.t.1</source_team_key>" +
+        "</transaction_data></player>" +
+        "</players></transaction></fantasy_content>"
     );
   });
 });
